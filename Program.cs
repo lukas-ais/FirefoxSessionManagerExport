fee3774fb385b67c4d52777f2cf1175382384490
Grützmacher <GRUETZMACHER@de-ais.r-r.local>
2017-11-18 12:13:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// <copyright>
+//     Copyright (c) Lukas Grützmacher. All rights reserved.
+// </copyright>
+
+namespace FirefoxSessionManagerExport
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    using Newtonsoft.Json.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            var file = args[0];
+            var text = File.ReadAllText(file);
+            var backup = JObject.Parse(text);
+
+            var root = backup["windows"].First;
+
+            var tabGroupsText = root["extData"]["tabview-group"].Value<string>();
+            var tabGroupsData = JObject.Parse(tabGroupsText);
+            var tabGroups = tabGroupsData.Children().ToDictionary(
+                x => x.First["id"].Value<string>(),
+                y => y.First["title"].Value<string>());
+
+            var tabsData = root["tabs"];
+            var tabs = tabsData
+                .Select(t =>
+                {
+                    var tabText = t["extData"]?["tabview-tab"].Value<string>();
+                    var groupId = tabGroups.First().Key;
+                    if (!string.IsNullOrWhiteSpace(tabText) && tabText != "null")
+                    {
+                        var tabData = JObject.Parse(tabText);
+                        groupId = tabData["groupID"].Value<string>();
+                    }
+
+                    var isPinned = t["pinned"]?.Value<bool>() ?? false;
+
+                    var entries = t["entries"];
+                    var urls = entries.Select(e => new
+                    {
+                        Title = e["title"].Value<string>(),
+                        Url = e["url"].Value<string>()
+                    });
+                    return new {GroupId = groupId, IsPinned = isPinned, Urls = urls.ToList()};
+                }).OrderBy(x => Convert.ToInt32(x.GroupId)).ToList();
+
+            var tabsByGroup = tabs.GroupBy(t => t.GroupId);
+
+            var htmlText = new StringBuilder();
+            htmlText.Append("<html><body>");
+
+            foreach (var tabGroup in tabsByGroup)
+            {
+                htmlText.Append($"<h1>{tabGroups[tabGroup.Key]}</h1><ul>");
+
+                foreach (var tab in tabGroup)
+                {
+                    htmlText.Append("<li>");
+                    if (tab.Urls.Count > 1)
+                    {
+                        htmlText.Append(tab.Urls.First().Title);
+                        if (tab.IsPinned)
+                        {
+                            htmlText.Append(" (pinned)");
+                        }
+
+                        htmlText.Append("<ul>");
+                        tab.Urls.ForEach(u => htmlText.Append($"<li><a href=\"{u.Url}\">{u.Title}</a></li>"));
+                        htmlText.Append("</ul>");
+                    }
+                    else
+                    {
+                        tab.Urls.ForEach(u => htmlText.Append($"<a href=\"{u.Url}\">{u.Title}</a>"));
+                    }
+
+                    htmlText.Append("</li>");
+                }
+
+                htmlText.Append("</ul>");
+            }
+
+            htmlText.Append("</body></html>");
+
+            var outputFile = file + ".html";
+            File.WriteAllText(outputFile, htmlText.ToString());
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿// <copyright>
+//     Copyright (c) Lukas Grützmacher. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FirefoxSessionManagerExport")]
+[assembly: AssemblyDescription("Simple exporter for URLs from deprecated Session Manager and Tab Groups")]
+[assembly: AssemblyProduct("FirefoxSessionManagerExport")]
+[assembly: AssemblyCopyright("Copyright © Lukas Grützmacher 2017")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eab1dda9-b36a-4d16-89fa-f08fc8b9beaa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
